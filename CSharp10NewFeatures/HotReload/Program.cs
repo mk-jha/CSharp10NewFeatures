be735af0c62f24d15cd7c7713aeeee9764263740
murarijha123 <murari.jha@kelltontech.com>
2022-01-17 22:45:59 +0530

    New features of .NET6 including C# 10

---
+++ CSharp10NewFeatures/HotReload/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+var x = new Utils();
+while (true)
+{
+    x.OutPut();
+}
+public class Utils
+{
+    public void OutPut()
+    {
+        Console.WriteLine("Changes on running");
+        Thread.Sleep(1000);
+    }
+
+}
---
+++ CSharp10NewFeatures/MinimalAPI/Program.cs
@@ -0,0 +1,11 @@
+// Just four lines code to create API endpoint
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World! Minimal API");
+
+// Creating New endpoint for demo
+
+app.MapGet("/getDetails", () => new { Name = "Murari jha", Address = "New Delhi", Contact = "888888" }); //done going to run now 
+
+app.Run();
