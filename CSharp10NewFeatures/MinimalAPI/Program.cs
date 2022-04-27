a7c8bf60e6423cdd67bfd097cbffaf63e5303379
murarijha123 <murari.jha@kelltontech.com>
2022-04-27 19:19:46 +0530

    Repository update

--- CSharp10NewFeatures/MinimalAPI/Program.cs
+++ CSharp10NewFeatures/MinimalAPI/Program.cs
@@ -7,5 +7,5 @@ app.MapGet("/", () => "Hello World! Minimal API");
 // Creating New endpoint for demo
 
 app.MapGet("/getDetails", () => new { Name = "Murari jha", Address = "New Delhi", Contact = "888888" }); //done going to run now 
-
+//
 app.Run();
